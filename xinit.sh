779c35647b4243ca1f63d643530dcf10866166b7
dragonsourced <dragonsourced@protonmail.com>
2020-02-12 13:06:33 -0500

    Initial commit

---
+++ xinit.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Turn off the connected HDMI ports, if any.
+xrandr --output HDMI-1 --off
+
+# shellcheck source=/home/Gordon/.fehbg
+[ -f ~/.fehbg ] && . ~/.fehbg
+
+compton -CczfbD1 -o 0.1
+redshift &
+dunst &
+
+if [ "$(date +%H)" -ge 16 ]; then
+    theme dark &
+else
+    theme light &
+fi
+
+sowm
+
+killall redshift
+killall bar.sh
