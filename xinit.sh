b8acf2d76ef4f47507a96d33133d42a0081c8dd0
dragonsourced <dragonsourced@protonmail.com>
2020-02-15 21:20:09 -0500

    Added compton

--- xinit.sh
+++ xinit.sh
@@ -6,8 +6,7 @@ xrandr --output HDMI-1 --off
 # shellcheck source=/home/Gordon/.fehbg
 [ -f ~/.fehbg ] && . ~/.fehbg
 
-# Uncomment if you want shadows & suchlike.
-# compton -CczfbD1 -o 0.1
+compton -CczfbD1 -o 0.1
 
 if [ "$(date +%H)" -ge 16 ]; then
     theme dark
